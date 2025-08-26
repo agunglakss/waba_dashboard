23483f4a4ff42729235036bf18cf51d7527c02e9
Agung Laksono <agung.laksono@Agungs-MacBook-Pro.local>
2025-08-26 17:09:16 +0700

    debug

--- src/app/api/analytics/detail/route.ts
+++ src/app/api/analytics/detail/route.ts
@@ -6,16 +6,16 @@ export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
     const wabaId = searchParams.get("wabaId") || "";
+    console.log(wabaId)
     const startDate = searchParams.get("startDate") || "";
     const endDate = searchParams.get("endDate") || "";
     
     const startUnixTimeStamp = startDate == "null" ? startTimeStamp : toTimeStampStartDate(startDate);
-    console.log("startUnixTimeStamp" + startTimeStamp)
     const endUnixTimeStamp = endDate == "null" ? endTimeStamp : toTimeStampEndDate(endDate);
 
     const data = await getSummaryAnalyticWithPhoneNumber({startDate: startUnixTimeStamp, endDate: endUnixTimeStamp, wabaId: wabaId});
     const wabaName  = await getWabaNameById(wabaId);
-    
+    console.log(data);
     return NextResponse.json(
       { data, "waba_name": wabaName, "waba_id": wabaId }
     );
